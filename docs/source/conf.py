9135489adbd0376504cd688f80fffc53cdb441b0
marpet <marco.peters@brockmann-consult.de>
2019-04-01 09:30:55 +0200

    set version 1.1.1-DEV

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Copernicus Programme'
 author = 'Brockmann Consult GmbH, Sinergise LTD., GeoVille GmbH'
 
 # The short X.Y version
-version = '1.1.0'
+version = '1.1.1'
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.1.1-DEV'
 
 
 # -- General configuration ---------------------------------------------------
